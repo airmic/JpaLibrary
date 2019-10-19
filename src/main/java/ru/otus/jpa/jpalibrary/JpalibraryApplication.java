3905d2fd3d05199b76c8c5a69260fa67232c67fb
She Mike <airmic@mail.ru>
2019-10-19 19:11:58 +0300

    jpa - init commit

---
+++ src/main/java/ru/otus/jpa/jpalibrary/JpalibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.jpa.jpalibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpalibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpalibraryApplication.class, args);
+    }
+
+}
